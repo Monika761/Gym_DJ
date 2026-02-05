77ad89ff151d47076af94142a0428e62a13f2d34
Monika761 <YOUR_GITHUB_EMAIL>
2026-02-05 14:52:03 +0530

    Fix static files path

--- gym_project1/settings.py
+++ gym_project1/settings.py
@@ -11,8 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,19 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-from pathlib import Path
-
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-import os
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'gymapp/static'),
-]
 
 
 
